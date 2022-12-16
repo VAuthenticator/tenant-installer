7063a6877f4f5b35bba05f689ceabf19c106fbdc
mrflick72 <valerio.vaudi@gmail.com>
2022-12-16 23:14:33 +0100

    update dynamo configuration

--- terraform/nosql/dynamo/dynamo.tf
+++ terraform/nosql/dynamo/dynamo.tf
@@ -32,23 +32,6 @@ resource "aws_dynamodb_table" "account_table" {
   }
 }
 
-resource "aws_dynamodb_table" "role_table" {
-  name         = var.role_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "role_name"
-
-  attribute {
-    name = "role_name"
-    type = "S"
-  }
-
-
-  tags = {
-    Name        = var.role_table_name
-    Environment = var.environment
-  }
-}
-
 resource "aws_dynamodb_table" "account_role_table" {
   name         = var.account_role_table_name
   billing_mode = "PAY_PER_REQUEST"
@@ -71,16 +54,50 @@ resource "aws_dynamodb_table" "account_role_table" {
   }
 }
 
-resource "aws_dynamodb_table" "keys_table" {
-  name         = var.keys_table_name
+resource "aws_dynamodb_table" "account_mfa_methods_table" {
+  name         = var.account_mfa_methods_table_name
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "master_key_id"
-  range_key    = "key_id"
+  hash_key     = "user_name"
+  range_key    = "mfa_method"
 
   attribute {
-    name = "master_key_id"
+    name = "user_name"
     type = "S"
   }
+
+  attribute {
+    name = "mfa_method"
+    type = "S"
+  }
+
+  tags = {
+    Name        = var.account_mfa_methods_table_name
+    Environment = var.environment
+  }
+}
+
+
+resource "aws_dynamodb_table" "role_table" {
+  name         = var.role_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "role_name"
+
+  attribute {
+    name = "role_name"
+    type = "S"
+  }
+
+
+  tags = {
+    Name        = var.role_table_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_dynamodb_table" "mfa_keys_table" {
+  name         = var.mfa_keys_table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key    = "key_id"
 
   attribute {
     name = "key_id"
@@ -88,14 +105,29 @@ resource "aws_dynamodb_table" "keys_table" {
   }
 
   tags = {
-    Name        = var.keys_table_name
+    Name        = var.mfa_keys_table
     Environment = var.environment
   }
 }
 
+resource "aws_dynamodb_table" "signature_keys_table" {
+  name         = var.signature_keys_table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key    = "key_id"
+
+  attribute {
+    name = "key_id"
+    type = "S"
+  }
+
+  tags = {
+    Name        = var.signature_keys_table
+    Environment = var.environment
+  }
+}
 
 resource "aws_dynamodb_table" "ticket_table" {
-  name         = var.mail_verification_ticket_table_name
+  name         = var.ticket_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "ticket"
 
@@ -113,7 +145,7 @@ resource "aws_dynamodb_table" "ticket_table" {
   }
 
   tags = {
-    Name        = var.mail_verification_ticket_table_name
+    Name        = var.ticket_table_name
     Environment = var.environment
   }
 }
