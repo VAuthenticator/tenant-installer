0c42888f99679c994b9c62a53d3189b1bfa01d65
mrflick72 <valerio.vaudi@gmail.com>
2023-02-24 01:44:45 +0100

    configurations

--- terraform/resources/terraform.tf
+++ terraform/resources/terraform.tf
@@ -8,4 +8,13 @@ terraform {
   }
 }
 
-provider "aws" {}
+provider "aws" {
+  region = "us-east-1"
+
+  endpoints {
+    s3       = "http://localhost:4566"
+    s3api    = "http://localhost:4566"
+    kms      = "http://localhost:4566"
+    dynamodb = "http://localhost:8000"
+  }
+}
